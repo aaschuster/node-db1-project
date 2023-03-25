f0f42efe67457ddca22c462df1aee27c8d244238
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-25 11:00:19 -0400

    refactor checkAccountPayload for better readability

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -3,32 +3,42 @@ exports.checkAccountPayload = (req, res, next) => {
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
 
-  if(!req.body.name || !req.body.budget) 
+  if(!req.body.name || !req.body.budget) {
     next({
       status: 400,
       message: "name and budget are required"
     });
-  else {
-    let { name, budget } = req.body;
-    name = name.trim();
+    return;
+  }
 
-    if(name.length < 3 || name.length > 100)
+  let name = req.body.name.trim();
+  const { budget } = req.body;
+
+  if(name.length < 3 || name.length > 100) {
       next({
         status: 400,
         message: "name of account must be between 3 and 100"
       });
-    else if(Number(budget) !== budget) {
-      next({
-        status: 400,
-        message: "budget of account must be a number"
-      })
-    } else if(budget < 0 || budget > 1000000) {
-        next({
-          status: 400,
-          message: "budget of account is too large or too small"
-        })
-      } else next();    
+      return;
   }
+
+  if(Number(budget) !== budget) {
+    next({
+      status: 400,
+      message: "budget of account must be a number"
+    })
+    return;
+  }  
+
+  if(budget < 0 || budget > 1000000) {
+    next({
+      status: 400,
+      message: "budget of account is too large or too small"
+    })
+    return;
+  }  
+
+  next();
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
